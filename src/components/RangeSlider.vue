f9fb3d3876cf2bebd596ffc84c120008fcdbb589
Dennis <dennisostendorf@web.de>
2019-03-28 18:34:26 +0100

    Built range slider and added test reactivity

---
+++ src/components/RangeSlider.vue
@@ -0,0 +1,28 @@
+<template>
+  <fieldset>
+    <label :for="id">{{ label }}</label>
+    <input
+      type="range"
+      :id="id"
+      name="rangeslider" 
+      min="0"
+      max="8"
+      value="0"
+      step="1"
+      @input="$emit('input', $event.target.value)"
+    >
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'RangeSlider',
+  props: [ "id", "label" ],
+  data: function() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Demand.vue
+++ src/views/Demand.vue
@@ -12,34 +12,62 @@
         :value="selectedRegion"
         @input="selectedRegion = $event"
       />
+      <RangeSlider
+        id="years"
+        label="Year"
+        @input="rangeValue = $event"
+      />
     </section>
     <section class="content">
       <p>Selected region: {{ selectedRegion }}</p>
+      <p>Selected year: {{ startyear + (yearinterval * rangeValue) }}</p>
+      <p>Fossil energy: {{ fossilAmount }} {{ unit }}</p>
+      <p>Non-fossil energy: {{ nonfossilAmount }} {{ unit }}</p>
+      <p>Total energy: {{ totalAmount }} {{ unit }}</p>
     </section>
   </article>
 </template>
 
 <script>
 import RadioSelect from '@/components/RadioSelect.vue'
+import RangeSlider from '@/components/RangeSlider.vue'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'demand',
   components: {
-    RadioSelect
+    RadioSelect,
+    RangeSlider
   },
   data: function () {
     return {
+      rangeValue: 0
     }
   },
   computed: {
     ...mapGetters([
+      'startyear',
+      'yearinterval',
+      'unit',
       'regions',
-      'selectedData'
+      'fossilData',
+      'totalData'
     ]),
+    fossilAmount: function () {
+      let fossilAmount = this.fossilData[0].values[this.rangeValue]
+      return fossilAmount
+    },
+    nonfossilAmount: function () {
+      let nonfossilAmount = this.totalData[0].values[this.rangeValue] - this.fossilData[0].values[this.rangeValue]
+      return nonfossilAmount
+    },
+    totalAmount: function () {
+      let totalAmount = this.totalData[0].values[this.rangeValue]
+      return totalAmount
+    },
     selectedRegion: {
       get () {
-        return this.$store.state.aggregated.selected.region // gets active region from store
+        return this.$store.state.selection.region // gets active region from store
       },
       set (value) {
         this.$store.commit('setRegion', value) // commits active region to store from :value
