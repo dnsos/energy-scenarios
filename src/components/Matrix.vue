24c85c9b87ff320a26e2d911323459bd95a1b45e
Dennis <dennisostendorf@web.de>
2019-04-23 17:20:59 +0200

    Fixed maxValue in Matrix

--- src/components/Matrix.vue
+++ src/components/Matrix.vue
@@ -6,7 +6,7 @@
       :society="matrixData[4].ssp"
       :fossilData="matrixData[4].baseline.fossil.values[rangeValue]"
       :nonfossilData="matrixData[4].baseline.nonfossil.values[rangeValue]"
-      :maxValueTest="matrixMaxValue"
+      :maxValue="matrixMaxValue"
       :transform="'translate(' + translateValues(1,1) + ')'"
     />
     <TypeCircles
@@ -15,7 +15,7 @@
       :society="matrixData[0].ssp"
       :fossilData="matrixData[0].baseline.fossil.values[rangeValue]"
       :nonfossilData="matrixData[0].baseline.nonfossil.values[rangeValue]"
-      :maxValueTest="matrixMaxValue"
+      :maxValue="matrixMaxValue"
       :transform="'translate(' + translateValues(1,5) + ')'"
     />
     <TypeCircles
@@ -24,7 +24,7 @@
       :society="matrixData[2].ssp"
       :fossilData="matrixData[2].baseline.fossil.values[rangeValue]"
       :nonfossilData="matrixData[2].baseline.nonfossil.values[rangeValue]"
-      :maxValueTest="matrixMaxValue"
+      :maxValue="matrixMaxValue"
       :transform="'translate(' + translateValues(5,1) + ')'"
     />
     <TypeCircles
@@ -33,7 +33,7 @@
       :society="matrixData[3].ssp"
       :fossilData="matrixData[3].baseline.fossil.values[rangeValue]"
       :nonfossilData="matrixData[3].baseline.nonfossil.values[rangeValue]"
-      :maxValueTest="matrixMaxValue"
+      :maxValue="matrixMaxValue"
       :transform="'translate(' + translateValues(5,5) + ')'"
     />
     <TypeCircles
@@ -42,7 +42,7 @@
       :society="matrixData[1].ssp"
       :fossilData="matrixData[1].baseline.fossil.values[rangeValue]"
       :nonfossilData="matrixData[1].baseline.nonfossil.values[rangeValue]"
-      :maxValueTest="matrixMaxValue"
+      :maxValue="matrixMaxValue"
       :transform="'translate(' + translateValues(3,3) + ')'"
     />
   </g>
--- src/components/TypeCircles.vue
+++ src/components/TypeCircles.vue
@@ -4,10 +4,10 @@
       <!--<EnergyCircle :transform="'translate(' + (-maxRadius * 2) + ',0)'" :maxRadius="maxRadius * 2" :value="fossilData + nonfossilData" :maxValue="maxValue * 2" />-->
     </g>
     <g transform="rotate(180)">
-      <EnergyCircle :maxRadius="maxRadius" :value="fossilData" :maxValue="maxValueTest" />
+      <EnergyCircle :maxRadius="maxRadius" :value="fossilData" :maxValue="maxValue" />
     </g>
     <g>
-      <EnergyCircle :maxRadius="maxRadius" :value="nonfossilData" :maxValue="maxValueTest" />
+      <EnergyCircle :maxRadius="maxRadius" :value="nonfossilData" :maxValue="maxValue" />
     </g>
     <text class="matrix__society" :transform="'translate(0,' + height * 0.5 + ')'">{{ society }}</text>
   </g>
@@ -21,16 +21,13 @@ export default {
   components: {
     EnergyCircle
   },
-  props: [ "width", "height", "society", "fossilData", "nonfossilData", "maxValueTest" ],
+  props: [ "width", "height", "society", "fossilData", "nonfossilData", "maxValue" ],
   data: function() {
     return {}
   },
   computed: {
     maxRadius: function () {
       return this.width / 4
-    },
-    maxValue: function () {
-      return 1004.921 // TODO: automate this value
     }
   }
 }
